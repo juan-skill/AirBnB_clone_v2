3718e9ba3c724a6ba1a005e3274fb5dc61a601be
juan-skill <1294@holbertonschool.com>
2020-08-16 23:23:07 +0000

    [feature] DBStorage - Amenity... and BOOM!: What’s a Many-to-Many relationship? 	modified:   models/__init__.py 	modified:   models/amenity.py 	modified:   models/base_model.py 	modified:   models/engine/db_storage.py 	modified:   models/place.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,20 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy.orm import relationship, backref
+from os import getenv
+
+from models.place import place_amenity
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """ doc """
+    __tablename__ = "amenities"
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+
+        place_amenities = relationship("Place", secondary=place_amenity)
+    else:
+        name = ""
--- models/place.py
+++ models/place.py
@@ -1,14 +1,30 @@
 #!/usr/bin/python3
-""" Place Module for HBNB project """
+"""This is the place class"""
+
 from models.base_model import BaseModel, Base
+from models.user import User
+from models.city import City
+import models
 
 from sqlalchemy import Column, Integer, String, Float, ForeignKey
 from sqlalchemy.orm import relationship, backref
 from os import getenv
 
+from sqlalchemy import *
+
+metadata = Base.metadata
+
+place_amenity = Table(
+    'place_amenity', metadata,
+    Column('place_id', String(60), ForeignKey('places.id'),
+           nullable=False, primary_key=True),
+    Column('amenity_id', String(60), ForeignKey('amenities.id'),
+           nullable=False, primary_key=True))
+
 
 class Place(BaseModel, Base):
     """ A place to stay """
+
     __tablename__ = "places"
     if getenv('HBNB_TYPE_STORAGE') == 'db':
 
@@ -23,8 +39,9 @@ class Place(BaseModel, Base):
         latitude = Column(Float, nullable=True)
         longitude = Column(Float, nullable=True)
         amenity_ids = []
-
         reviews = relationship("Review", passive_deletes=True, backref="place")
+        amenities = relationship(
+            "Amenity", secondary=place_amenity, viewonly=False)
     else:
         city_id = ""
         user_id = ""
@@ -51,3 +68,24 @@ class Place(BaseModel, Base):
                     my_list.append(review)
 
             return my_list
+
+        @property
+        def amenities(self):
+            """
+            returns the list of Amenity instances based on the
+            attribute amenity_ids
+            """
+            my_dict = models.storage.all('Amenity')
+            for amenity in my_dict.values():
+                if amenity.place_id == self.id:
+                    amenity_ids.append(amenity)
+            return self.amenity_ids
+
+        @amenities.setter
+        def amenities(self, obj=None):
+            """
+            handles append method for adding an Amenity.id to the
+            attribute amenity_ids
+            """
+            if type(obj) == 'Amenity':
+                self.amenity_ids.append(obj.id)
