6b73aeb37e291aa67060b69b05d174dc1aa47527
juan-skill <1294@holbertonschool.com>
2020-09-02 17:47:14 +0000

    [add] List of states: Integrates FLASk web application to AirBnB project new file:   web_flask/7-states_list.py new file:   web_flask/templates/7-states_list.html

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" starts a flask web application """
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.teardown_appcontext
+def close_session(self):
+    """ remove the current SQLAlchemy Session """
+    storage.close()
+
+
+@app.route('/states_list')
+def states_list():
+    """ return the list of city objects """
+    return render_template('7-states_list.html',
+                           states=storage.all(State).values())
+
+
+if __name__ == '__main__':
+
+    app.run(debug=True, host='0.0.0.0', port='5000')
